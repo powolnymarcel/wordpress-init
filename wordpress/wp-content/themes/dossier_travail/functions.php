db8a4dde40f49265aea4e3f9d036f05a1252260d
powolnymarcel <powolnymarcel@gmail.com>
2016-04-19 14:21:11 +0200

    definition de constantes, header.php, functions.php + ajout icons

---
+++ wordpress/wp-content/themes/dossier_travail/functions.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * functions.php
+ *
+ * Les fn du theme
+ *
+ */
+
+
+/*
+*
+*1. CONSTANTES
+*
+*/
+// Defini une constante THEMEROOT pour uri des css
+define('THEMEROOT',get_stylesheet_directory_uri());
+define('IMAGES',THEMEROOT.'/img');
+define('JS',THEMEROOT.'/js');
+
+
+?>
---
+++ wordpress/wp-content/themes/dossier_travail/index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * index.php
+ *
+ * Le template principal
+ *
+ */
+?>
+
+<?php
+get_header()
+/*
+*
+*
+*
+*/
+?>
